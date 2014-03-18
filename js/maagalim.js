e1ec360e780356e103316da6fa34410474cdb94c
Daniel Prinz <danprinz@gmail.com>
2014-03-18 23:21:12 +0200

    More wizard - scroll-spy

--- js/maagalim.js
+++ js/maagalim.js
@@ -3,15 +3,6 @@ var controllers = angular.module('controllers', []);
 
 controllers.controller('dashboardController', function($scope, $http) {
     console.log('dashboardController init called');
-    var students = {};
-
-    Class = function(id, age, school, number, studentsCount) {
-        this.id = id;
-        this.age = age;
-        this.school = school;
-        this.number = number;
-        this.studentsCount = studentsCount;
-    }
 
     $http.get('/_Details/_Dashboard')
          .success(function(data) {
@@ -54,31 +45,37 @@ controllers.controller('dashboardController', function($scope, $http) {
     $scope.addFeature('נעילת חודש', '72%', ['קצת פירוט על מה חסר', 'ומה דרוש להשלמה']);
 });
 
-controllers.controller('userController', function($scope, $http) {
+controllers.controller('userController', function ($scope) {
     console.log('userController init called');
     $scope.guider = {lastName: 'כהן', firstName: 'משה', id: 1234};
 
-    var messages = [{id: 1234, from: 'דוד', time: new Date(), message: 'מי שלח אותך?', seen: true},
-                    {id: 4321, from: 'משה', time: new Date(), message: 'מה עם הדוחות?', seen: true}];
-
-    $scope.messages = messages;
+    $scope.messages = [
+        {id: 1234, from: 'דוד', time: new Date(), message: 'מי שלח אותך?', seen: true},
+        {id: 4321, from: 'משה', time: new Date(), message: 'מה עם הדוחות?', seen: true}
+    ];
 });
 
-controllers.controller('wizardController', function($scope) {
+controllers.controller('wizardController', function ($scope, $timeout, $location, $anchorScroll) {
     console.log('wizardController created');
+
+    $scope.goto = function(id) {
+        console.log('scrolling to id ' + id);
+        $location.hash(id);
+        $anchorScroll();
+    };
 
     var wizard_steps = [];
 
-    WizardStep = function(name, predecessors) {
+    var WizardStep = function (name, predecessors) {
         this.name = name;
         this.predecessors = predecessors;
         this.percentage = 0;
-        this.complete = function() {
+        this.complete = function () {
             return this.percentage == 100;
         };
-    }
+    };
 
-    WizardDatesStep = function() {
+    var WizardDatesStep = function () {
         WizardStep.call(this, 'Dates', []);
         var self = this;
 
@@ -86,16 +83,16 @@ controllers.controller('wizardController', function($scope) {
         this.wizard_date = undefined;
         this.wizard_class = undefined;
 
-        this.today = function() {
+        this.today = function () {
             this.wizard_date = new Date();
         };
 
         // Disable weekend selection
-        this.disabled = function(date, mode) {
+        this.disabled = function (date, mode) {
             return ( mode === 'day' && ( date.getDay() === 5 || date.getDay() === 6 ) );
         };
 
-        this.openDate = function($event) {
+        this.openDate = function ($event) {
             $event.preventDefault();
             $event.stopPropagation();
 
@@ -111,36 +108,43 @@ controllers.controller('wizardController', function($scope) {
 
         this.date_format = 'dd/MM/yyyy';
 
-        this.change = function() {
+        this.change = function () {
             console.log('change dates...');
             var filled = 0;
-            if (self.wizard_date) {
-                filled++;
-            }
-            if (self.wizard_class) {
-                filled++;
-            }
-            var percentage = Math.round((filled / self.STEP_TOTAL_FIELDS) * 100);
-            self.percentage = percentage;
-            wizard_changed();
+            if (self.wizard_date)  filled++;
+            if (self.wizard_class) filled++;
+            self.percentage = Math.round((filled / self.STEP_TOTAL_FIELDS) * 100);
         }
-    }
+    };
 
-    WizardHoursStep = function() {
+    var WizardHoursStep = function () {
         WizardStep.call(this, 'Hours', ['Dates']);
         var self = this;
 
-        this.STEP_TOTAL_FIELDS = 1;
         this.wizard_hours = undefined;
-        this.change = function() {
+        this.wizard_km = undefined;
+        this.wizard_trans = undefined;
+        this.wizard_other_refunds = undefined;
+        this.wizard_other_refunds_notes = '';
+
+        this.STEP_TOTAL_FIELDS = 4;
+        this.change = function () {
             console.log('hours change...');
             var filled = 0;
-            if (self.wizard_hours) {
-                filled++;
+            if (self.wizard_hours || self.wizard_hours == 0)         filled++;
+            if (self.wizard_km || self.wizard_km == 0)            filled++;
+            if (self.wizard_trans || self.wizard_trans == 0)         filled++;
+            if (self.wizard_other_refunds || self.wizard_other_refunds == 0) {
+                if (self.wizard_other_refunds == 0) {
+                    filled++;
+                }
+                else {
+                    if (self.wizard_other_refunds_notes && self.wizard_other_refunds_notes.trim() != '') {
+                        filled++;
+                    }
+                }
             }
-            var percentage = Math.round((filled / self.STEP_TOTAL_FIELDS) * 100);
-            self.percentage = percentage;
-            wizard_changed();
+            self.percentage = Math.round((filled / self.STEP_TOTAL_FIELDS) * 100);
         }
     }
 
@@ -149,58 +153,77 @@ controllers.controller('wizardController', function($scope) {
 
     var dates_step = new WizardDatesStep();
     $scope.dates = dates_step;
-    $scope.$watch('dates', dates_step.change, true);
+    $scope.$watch('dates', function() {
+        dates_step.change();
+        $scope.wizard_changed();
+    }, true);
 
     var hours_step = new WizardHoursStep();
     $scope.hours = hours_step;
-    $scope.$watch('hours', hours_step.change, true);
+    $scope.$watch('hours', function() {
+        hours_step.change();
+        $scope.wizard_changed();
+    }, true);
 
     $scope.wizard_steps = wizard_steps;
     $scope.wizard_steps.push(dates_step);
     $scope.wizard_steps.push(hours_step);
 
-    var find_step = function(name) {
+    var find_step = function (name) {
         for (var idx = 0; idx < $scope.wizard_steps.length; ++idx) {
             if ($scope.wizard_steps[idx].name == name) {
                 return $scope.wizard_steps[idx];
             }
         }
-    }
+        return undefined;
+    };
 
     $scope.wizard_percentage = 0;
-    var wizard_changed = function() {
-        $scope.wizard_percentage = 0;
-        $.each(wizard_steps, function(index, step) {
-            $scope.wizard_percentage += (step.percentage / wizard_steps.length);
+    $scope.wizard_changed = function () {
+        var percentage = 0;
+        $.each(wizard_steps, function (index, step) {
+            percentage += (step.percentage / wizard_steps.length);
             var enabled = true;
-            $.each(step.predecessors, function(index, pre_step_name) {
+            $.each(step.predecessors, function (index, pre_step_name) {
                 var pre_step = find_step(pre_step_name);
                 if (pre_step && pre_step.complete() == false) {
                     enabled = false;
                 }
             });
+            var step_selector = 'div [step="' + step.name + '"]';
             if (enabled) {
-                $('div [step="' + step.name + '"]').find('*').prop('disabled',false);
-                $('div [step="' + step.name + '"]').css('background-color', '').fadeTo(200, 1);
+                $(step_selector).find('*').prop('disabled', false);
+                $(step_selector).css('background-color', '').fadeTo(200, 1);
             }
             else {
-                $('div [step="' + step.name + '"]').find('*').prop('disabled',true);
-                $('div [step="' + step.name + '"]').css('background-color', 'gray').fadeTo(200, 0.3);
+                $(step_selector).find('*').prop('disabled', true);
+                $(step_selector).css('background-color', 'gray').fadeTo(200, 0.3);
             }
         });
-    }
-    $scope.wizard_progress_color = $scope.wizard_percentage > 10 ? '#ffffff' : '#000000';
+        $scope.wizard_percentage = percentage;
+        console.log('$scope.wizard_percentage = ' + $scope.wizard_percentage);
+        $scope.wizard_progress_color = percentage > 10 ? '#ffffff' : '#000000';
+        //$scope.wizard_progress_type = calc_wizard_progress_type(percentage);
+    };
+
     var progress_types = {30: 'danger', 50: 'warning', 90: 'primary', 100: 'success'};
-    $scope.wizard_progress_type = function() {
-        bot_range = 0;
-        for (up_range in progress_types) {
+    $scope.calc_wizard_progress_type = function() {
+        var bot_range = -1;
+        for (var up_range in progress_types) {
             if ($scope.wizard_percentage > bot_range && $scope.wizard_percentage <= up_range) {
+                console.log('in range - wizard_progress_type() return ' + progress_types[up_range]);
                 return progress_types[up_range];
             }
             bot_range = up_range;
         }
-        return progress_types[0];
+        console.log('not in range - wizard_progress_type() return ' + progress_types[30]);
+        return progress_types[30];
     }
+    //$timeout(function() { console.log('changing %...'); $scope.wizard_changed();}, 1);
+
+//    dates_step.wizard_date = new Date();
+//    dates_step.wizard_class = 'כיתה א';
+//    hours_step.wizard_other_refunds = 2;
 
 });
 
@@ -237,7 +260,7 @@ Number.prototype.pad = function(size) {
 }
 
 Date.prototype.string = function(with_time) {
-    return this.getDay().pad(2) + '/' + (this.getMonth()+1).pad(2) + '/' + (this.getYear()+1900) +
+    return this.getDate().pad(2) + '/' + (this.getMonth()+1).pad(2) + '/' + (this.getYear()+1900) +
            (with_time ? (' ' + this.getHours().pad(2) + ':' + this.getMinutes().pad(2) + ':' + this.getSeconds().pad(2)) : '');
 }
 
